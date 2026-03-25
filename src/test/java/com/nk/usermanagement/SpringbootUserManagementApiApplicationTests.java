6d7a1cbc3b86f7318a63a0fb2baaa176c77be415
nkmevada <nkmevada2801@gmail.com>
2026-03-25 17:02:32 -0400

    Initial commit

---
+++ src/test/java/com/nk/usermanagement/SpringbootUserManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nk.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUserManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
