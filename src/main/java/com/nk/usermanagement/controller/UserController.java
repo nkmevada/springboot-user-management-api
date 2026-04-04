339e5914f3e3b63b460caf3863848277b2900fa1
nkmevada <nkmevada2801@gmail.com>
2026-04-04 11:25:00 -0400

    feat: add validation and global exception handling for user API

--- src/main/java/com/nk/usermanagement/controller/UserController.java
+++ src/main/java/com/nk/usermanagement/controller/UserController.java
@@ -4,9 +4,7 @@ import com.nk.usermanagement.dto.request.UserRequest;
 import com.nk.usermanagement.dto.response.UserResponse;
 import com.nk.usermanagement.entity.User;
 import com.nk.usermanagement.service.UserService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +21,7 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest userRequest){
+    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest userRequest){
         UserResponse savedUser = userService.createUser(userRequest);
         return ResponseEntity.ok(savedUser);
     }
@@ -34,19 +32,13 @@ public class UserController {
         return ResponseEntity.ok(user);
     }
 
-//    @GetMapping
-//    public ResponseEntity<List<User>> getAllUser(){
-//        return ResponseEntity.ok(userService.getAllUser());
-//    }
     @GetMapping
-    public ResponseEntity<List<UserResponse>> getAllUser(@RequestParam(defaultValue = "0") int page,
-                                                         @RequestParam(defaultValue = "2") int size){
-        Pageable pageable = PageRequest.of(page,size);
-        return ResponseEntity.ok(userService.getAllUser(pageable));
+    public ResponseEntity<List<User>> getAllUser(){
+        return ResponseEntity.ok(userService.getAllUser());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @RequestBody UserRequest userRequest){
+    public ResponseEntity<UserResponse> updateUser(@Valid @PathVariable Long id, @RequestBody UserRequest userRequest){
         UserResponse updatedUser = userService.updateUser(id,userRequest);
         return ResponseEntity.ok(updatedUser);
     }
