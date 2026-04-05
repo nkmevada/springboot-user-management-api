0073e1aad3b06f6781ca339aebce279a078e8891
nkmevada <nkmevada2801@gmail.com>
2026-04-05 11:18:00 -0400

    feat: add pagination support to get all users API

--- src/main/java/com/nk/usermanagement/repository/UserRepository.java
+++ src/main/java/com/nk/usermanagement/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.nk.usermanagement.repository;
 
 import com.nk.usermanagement.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
