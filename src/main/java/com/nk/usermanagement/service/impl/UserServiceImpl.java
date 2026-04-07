5f7539d79fe632009f5c2c7849734298aeef2015
nkmevada <nkmevada2801@gmail.com>
2026-04-07 10:13:13 -0400

    feat: add logging to controller, service

--- src/main/java/com/nk/usermanagement/service/impl/UserServiceImpl.java
+++ src/main/java/com/nk/usermanagement/service/impl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import com.nk.usermanagement.exception.ResourceNotFoundException;
 import com.nk.usermanagement.mapper.UserMapper;
 import com.nk.usermanagement.repository.UserRepository;
 import com.nk.usermanagement.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -19,6 +21,7 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
@@ -31,6 +34,7 @@ public class UserServiceImpl implements UserService {
         user.setName(userRequest.getName());
         user.setEmail(userRequest.getEmail());
         User savedUser = userRepository.save(user);
+        logger.info("Creating user with email: {}", userRequest.getEmail());
         return userMapper.toResponse(savedUser);
     }
 
@@ -43,6 +47,7 @@ public class UserServiceImpl implements UserService {
     public UserResponse getUserById(Long id) {
         //return userRepository.findById(id).orElseThrow(()->new RuntimeException("User not found"));
         User user = userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User not found with id :" + id));
+        logger.info("Get user data with id: {}",id);
         return userMapper.toResponse(user);
     }
 
@@ -52,11 +57,13 @@ public class UserServiceImpl implements UserService {
         existingUser.setName(userRequest.getName());
         existingUser.setEmail(userRequest.getEmail());
         User savedUpdatedUser = userRepository.save(existingUser);
+        logger.info("Update user data with id: {}",id);
         return userMapper.toResponse(savedUpdatedUser);
     }
 
     @Override
     public void deleteUser(Long id) {
+        logger.info("Delete user data with id: {}",id);
         userRepository.deleteById(id);
     }
 
@@ -74,6 +81,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<UserResponse> searchUser(String keyword) {
         List<User> users = userRepository.findByNameContainingIgnoreCase(keyword);
+        logger.info("Search username: {}",keyword);
         return users.stream().map(userMapper::toResponse).toList();
     }
 
